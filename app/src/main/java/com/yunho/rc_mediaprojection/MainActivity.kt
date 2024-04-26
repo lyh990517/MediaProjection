8085b8c8537e9c83e1243ac0696d7535e397b28d
lyh990517 <lyh990517@naver.com>
2024-04-26 10:22:54 +0900

    fix

--- app/src/main/java/com/yunho/rc_mediaprojection/MainActivity.kt
+++ app/src/main/java/com/yunho/rc_mediaprojection/MainActivity.kt
@@ -4,24 +4,33 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import com.yunho.mediaprojection.MediaProjectionFactory
 import com.yunho.mediaprojection.MediaProjectionState
 import com.yunho.rc_mediaprojection.ui.theme.RCMediaProjectionTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     private val mediaProjection = MediaProjectionFactory.create()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        lifecycleScope.launch {
+            mediaProjection.getMediaProjection().collect {
+                when (it) {
+                    is MediaProjectionState.Error -> Log.e("getProjection", it.throwable.message ?: "")
+                    MediaProjectionState.Idle -> {}
+                    is MediaProjectionState.Success -> Log.e("getProjection", it.toString())
+                }
+            }
+        }
         setContent {
             RCMediaProjectionTheme {
                 // A surface container using the 'background' color from the theme
@@ -29,17 +38,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    LaunchedEffect(Unit) {
-                        mediaProjection.getMediaProjection().collect {
-                            when (it) {
-                                is MediaProjectionState.Error -> Log.e("getProjection", it.throwable.message ?: "")
-                                MediaProjectionState.Idle -> {}
-                                is MediaProjectionState.Success -> Log.e("getProjection", it.toString())
-                            }
-                        }
-                    }
-                    Column(Modifier.fillMaxSize()) {
-                    }
                 }
             }
         }
